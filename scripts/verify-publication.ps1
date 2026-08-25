6b59947ebbdb7840ea159315d35ccf0ec8a367b6
Iron-Wph <2779056435@qq.com>
2026-08-25 16:14:55 +0800

    meta: Tighten publication hygiene checks

--- scripts/verify-publication.ps1
+++ scripts/verify-publication.ps1
@@ -18,7 +18,7 @@ $blockedExtensions = @(
     '.dll', '.so', '.dylib', '.pyd', '.node', '.obj', '.o', '.lib', '.pdb',
     '.ipch', '.pch', '.class', '.jar'
 )
-$blockedPathPattern = '(?i)(^|/)(node_modules|venv|\.venv|site-packages|__pycache__|\.vs|\.idea|\.settings|\.lift|\.next)(/|$)'
+$blockedPathPattern = '(?i)(^|/)(node_modules|venv|\.venv|site-packages|__pycache__|\.vs|\.idea|\.settings|\.lift|\.vscode|\.next)(/|$)'
 
 $blockedFiles = [System.Collections.Generic.List[string]]::new()
 $largeFiles = [System.Collections.Generic.List[string]]::new()
