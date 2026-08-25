df8dd5fee2c60116edef2af26a9c06cdb3d8ad35
Iron-Wph <2779056435@qq.com>
2026-08-25 16:18:04 +0800

    meta: Exclude executable output directories

--- scripts/verify-publication.ps1
+++ scripts/verify-publication.ps1
@@ -18,7 +18,7 @@ $blockedExtensions = @(
     '.dll', '.so', '.dylib', '.pyd', '.node', '.obj', '.o', '.lib', '.pdb',
     '.ipch', '.pch', '.class', '.jar'
 )
-$blockedPathPattern = '(?i)(^|/)(node_modules|venv|\.venv|site-packages|__pycache__|\.vs|\.idea|\.settings|\.lift|\.vscode|\.next)(/|$)'
+$blockedPathPattern = '(?i)(^|/)(node_modules|venv|\.venv|site-packages|__pycache__|\.vs|\.idea|\.settings|\.lift|\.vscode|exe|\.next)(/|$)'
 
 $blockedFiles = [System.Collections.Generic.List[string]]::new()
 $largeFiles = [System.Collections.Generic.List[string]]::new()
